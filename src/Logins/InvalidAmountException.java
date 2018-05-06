72b2d5335dc81367799488d79c928706b48ab591
Bogdan Pașca <bogdan_hunt2003@yahoo.com>
2018-05-06 12:39:39 +0300

    proiect final - mysql si gui

--- src/Logins/InvalidAmountException.java
+++ src/Logins/InvalidAmountException.java
@@ -1,19 +1,8 @@
 package Logins;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Carl
- */
 class InvalidAmountException extends Exception {
 
     public InvalidAmountException() {
         super("Fonduri insuficiente pentru a putea finaliza tranzacția.");
     }
-    
 }
